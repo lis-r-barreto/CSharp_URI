e5e940650c056b38927d020eba215185269b87a1
lis-r-barreto <lis_barreto@outlook.com>
2020-07-26 14:02:29 -0300

    Adicionadas novas soluções

---
+++ URI1143/URI1143/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace URI1143
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i=1; i<=N; i++)
+            {
+                int quadrado = (int) Math.Pow(i, 2);
+                int cubo = (int) Math.Pow(i, 3);
+
+                Console.WriteLine(i + " " + quadrado + " " + cubo);
+            }
+        }
+    }
+}
